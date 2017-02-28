aec6a384b03b3d762968bf0e8840ec9dca37d1d9
Dreamer <wuhuanhost@users.noreply.github.com>
2017-02-28 17:57:19 +0800

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,37 @@
+/**
+ * 10进制数字转64进制字符串
+ * @param  {[type]} number [description]
+ * @return {[type]}        [description]
+ */
+function number10to64(number) {
+    var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ-_'.split(''),
+        radix = chars.length,//基数
+        qutient = number,//系数
+        arr = [];
+    do {
+        mod = qutient % radix;
+        qutient = (qutient - mod) / radix;
+        arr.unshift(chars[mod]);
+    } while (qutient);
+
+    return arr.join("");
+}
+
+
+/**
+ * 64进制字符串转10进制数字
+ * @param  {[type]} number_code [description]
+ * @return {[type]}             [description]
+ */
+function string64to10(number_code) {
+  var chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ_-',
+    radix = chars.length,
+    number_code = String(number_code),
+    len = number_code.length,
+    i = 0,
+    origin_number = 0;
+  while (i < len) {
+    origin_number += Math.pow(radix, i++) * chars.indexOf(number_code.charAt(len - i) || 0);
+  }
+  return origin_number;
+}
