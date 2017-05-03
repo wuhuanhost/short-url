90ad012198c1f75847d387cf1310e48275ba4abb
wuhuanhost <post.thinking@gmail.com>
2017-05-03 19:26:20 +0800

    update

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,11 @@
 var Router = require('koa-router');
 var router = new Router();
 
-
 router.get('/', function (ctx, next) {
 	console.log(12321);
-	ctx.render('body',{title:"hello world!!!"});
+	ctx.render('template',{title:"hello world!!!"});
+	next();
 });
  
+
 module.exports = router;
