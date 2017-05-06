a102959b552a50bf1127d5e5617535454eaf8c7e
wuhuanhost <post.thinking@gmail.com>
2017-05-06 09:53:20 +0800

    修改了端口和日志目录位置

--- config/log_config.js
+++ config/log_config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 //日志根目录
-var baseLogPath = path.resolve(__dirname, '../../logs')
+var baseLogPath = path.resolve(__dirname, '../logs')
 
 //错误日志目录
 var errorPath = "/error";
